b57e458bdd42148f58c2a3c65f27730e985f6e93
anthonykhong <anthonykhong95200@gmail.com>
2023-03-03 23:51:58 +0100

    Added images

--- js/main.js
+++ js/main.js
@@ -1,9 +1,42 @@
 /*----- constants -----*/
 
 /*----- app's state (variables) -----*/
+let scores;
+
+let results;
+
+let winner;
 
 /*----- cached elements  -----*/
 
 /*----- event listeners -----*/
 
 /*----- functions -----*/
+init();
+
+function init() {
+  scores = {
+    p: 0,
+    t: 0,
+    c: 0,
+  };
+  results = {
+    p: "r",
+    c: "r",
+  };
+  winner = "t";
+  render();
+}
+
+function renderScores() {
+  for (let key in scores) {
+    const scoreEl = document.getElementById(`${key}-score`);
+    scoreEl.innerText = scores[key];
+  }
+}
+function renderResults() {}
+
+function render() {
+  renderScores();
+  renderResults();
+}
