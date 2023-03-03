02d9da136f9a8cc337452aa7361b030c140078c0
anthonykhong <anthonykhong95200@gmail.com>
2023-03-04 00:24:15 +0100

    Update javascript

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 /*----- constants -----*/
-
+const RPS_LOOKUP = {
+  r: "/imgs/rock.png",
+  p: "/imgs/paper.png",
+  s: "/imgs/scissors.png",
+};
 /*----- app's state (variables) -----*/
 let scores;
 
@@ -8,8 +12,11 @@ let results;
 let winner;
 
 /*----- cached elements  -----*/
+const pResultEl = document.getElementById("p-result");
+const cResultEl = document.getElementById("c-result");
 
 /*----- event listeners -----*/
+document.querySelector("main").addEventListener("click", handleChoice);
 
 /*----- functions -----*/
 init();
@@ -28,13 +35,28 @@ function init() {
   render();
 }
 
+function handleChoice(evt) {
+  // Guards
+  if (evt.target.tagName !== "BUTTON") return;
+  // Player has made a choice
+  results.p = evt.target.innerText.toLowerCase();
+  results.c = getRandomRPS();
+
+  render();
+}
+
+function getRandomRPS() {}
+
 function renderScores() {
   for (let key in scores) {
     const scoreEl = document.getElementById(`${key}-score`);
     scoreEl.innerText = scores[key];
   }
 }
-function renderResults() {}
+function renderResults() {
+  pResultEl.src = RPS_LOOKUP[results.p];
+  cResultEl.src = RPS_LOOKUP[results.c];
+}
 
 function render() {
   renderScores();
